1a94538e045e9018df5cd178778349bd0380f353
Krizel <krizzlybear@Minnemac.local>
2020-01-11 20:38:21 -0800

    adding pseudocode to outline project

---
+++ bamazonCustomer.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const inquirer = require ("inquirer");
+
+// basis of app: Amazon-like storefront. Take in orders and deplete stock from the store's inventory.
+
+// create the connection for the sql db
+
+// connect to the mysql server and sql database
+
+// list all the products
+
+// prompt user what they'd like to purchase using the id number
+
+// quantity of purchase (must be a number)
+
+// is there anything else? y/n
+
+// if else statement. if yes - open list of products; if no - show receipt
